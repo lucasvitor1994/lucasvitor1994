9212a2e2203f4f1ad9dd02cca1c9e3dda88a6aff
lucasvitor1994 <55961366+lucasvitor1994@users.noreply.github.com>
2021-03-23 08:32:27 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,44 @@
+const fs = require ("fs")
+
+const csv2json = require ('/Users/lucas/Desktop/aula/insight/src/csv2json')
+
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : '192.168.30.78',
+  port     : 3306,
+  user     : 'root',
+  password : 'vikstar.2019',
+  database : 'storages',
+  keepAlive: 240000
+});
+
+
+connection.connect(function(err, res, row){
+  if(err) return console.log(err);
+  console.log('conectado Puffin');
+})
+
+fs.readFile('public/documents/mapping-basicrecord.txt', 'utf-8', function (err, data){
+    // console.log(data)
+    const TXT2 = data.replace(/;/g, '') 
+  
+    const text = TXT2.split(/\r?\n/)
+    const linhas =(text.length)
+    console.log(linhas)
+   
+    
+    var insert;
+for (insert = 0; insert < 1000; insert++) {
+    
+    const ROWS = text[insert]
+    console.log(linhas + "linhas" + insert)
+
+    connection.query(`INSERT INTO primeiro(diretorio) VALUES ('${ROWS}')`, function (err, rows){
+        console.log(err)
+        console.log(rows)
+   })
+
+}
+
+
+})
